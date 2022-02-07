756b9eb5390240bf83b141f7f584726a38289727
Ji Xiang <jixiangchua2697@gmail.com>
2022-02-07 17:55:43 +0800

    Added Speech Recognition Function but not sure what causes the exception error. I have commented out the speech recognition function

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Recognition;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +21,35 @@ namespace EvernoteClone.View
     /// </summary>
     public partial class NotesWindow : Window
     {
+        SpeechRecognitionEngine recognizer;
+
         public NotesWindow()
         {
             InitializeComponent();
+
+            //var currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
+            //                     where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+            //                     select r).FirstOrDefault();
+
+            //recognizer = new SpeechRecognitionEngine(currentCulture);
+
+            //GrammarBuilder builder = new GrammarBuilder();
+            //builder.AppendDictation();
+            //Grammar grammar = new Grammar(builder);
+
+            ////Add grammar so that the recognizer can recognise the word you say
+            //recognizer.LoadGrammar(grammar);
+            //recognizer.SetInputToDefaultAudioDevice();
+            ////Subscribe the event SpeechRecognized with an event handler:
+            //recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //string recognizedText = e.Result.Text;
+
+            ////Adding the text result into the rich text box
+            //contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recognizedText)));
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -29,9 +57,18 @@ namespace EvernoteClone.View
             Application.Current.Shutdown();
         }
 
+        bool isRecognizing = false;
         private void SpeechButton_Click(object sender, RoutedEventArgs e)
         {
-
+            //if (!isRecognizing)
+            //{
+            //    recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            //    isRecognizing = true;
+            //} else
+            //{
+            //    recognizer.RecognizeAsyncStop();
+            //    isRecognizing = false;
+            //}
         }
 
         private void contentRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
