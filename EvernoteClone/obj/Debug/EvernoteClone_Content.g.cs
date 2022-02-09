8da1df70f1763232c0724d998806a34665e5fe39
Ji Xiang <jixiangchua2697@gmail.com>
2022-02-09 10:43:39 +0800

    Added Windows App Icon

--- EvernoteClone/obj/Debug/EvernoteClone_Content.g.cs
+++ EvernoteClone/obj/Debug/EvernoteClone_Content.g.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("view/evernoteclone_app_icon.ico")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("e_sqlite3.dll")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("e_sqlite3.dll")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("e_sqlite3.dll")]
